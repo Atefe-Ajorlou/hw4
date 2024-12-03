19c07bedf3e355f8d7bbf58c1b5ecddfd3ad2e56
Atefe <78861411+Atefe-Ajorlou@users.noreply.github.com>
2024-12-03 13:12:11 +0330

    Add files via upload

---
+++ hw4/exx.java
@@ -0,0 +1,15 @@
+package hw4;
+
+public class exx {
+
+	public static void main(String[] args) {
+
+		try {
+
+		}catch (Exception e) {
+			throw new FileNotFoundException("File not found");
+		}
+
+	}
+
+}
